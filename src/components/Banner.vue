51634db8d10fe64fd2f75f15ed7459ce0a10b629
Agrain7 <cgmaeng@ipil.co.kr>
2025-02-19 13:31:54 +0900

    frontend 20250219

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -36,12 +36,12 @@
 </template>
 
 <script>
-import banner1 from '@/assets/Banner1.jpg'
-import banner2 from '@/assets/Banner2.jpg'
-import banner3 from '@/assets/Banner3.jpg'
-import banner4 from '@/assets/Banner4.jpg'
-import banner5 from '@/assets/Banner5.jpg'
-import banner6 from '@/assets/Banner6.jpg'
+import banner1 from '@/assets/banner1.jpg'
+import banner2 from '@/assets/banner2.jpg'
+import banner3 from '@/assets/banner3.jpg'
+import banner4 from '@/assets/banner4.jpg'
+import banner5 from '@/assets/banner5.jpg'
+import banner6 from '@/assets/banner6.jpg'
 
 export default {
   name: "Banner",
