b9a12cf73a0d22cafe0d1ee9f1d78e67961e2a4a
Agrain7 <cgmaeng@ipil.co.kr>
2025-03-06 10:52:49 +0900

    스타일 업데이트 및 아이콘 추가

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -16,21 +16,21 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>9t이하</td>
-          <td>{{ priceConfig["비규격"]["9t이하"] }}원/kg</td>
-          <td>{{ priceConfig["중국산"]["9t이하"] }}원/kg</td>
-          <td>{{ priceConfig["SM275"]["9t이하"] }}원/kg</td>
-          <td>{{ priceConfig["SM355"]["9t이하"] }}원/kg</td>
-        </tr>
-        <tr>
-          <td>12~50t</td>
-          <td>{{ priceConfig["비규격"]["12~50t"] }}원/kg</td>
-          <td>{{ priceConfig["중국산"]["12~50t"] }}원/kg</td>
-          <td>{{ priceConfig["SM275"]["12~50t"] }}원/kg</td>
-          <td>{{ priceConfig["SM355"]["12~50t"] }}원/kg</td>
-        </tr>
-      </tbody>
+  <tr>
+    <td>12~50t</td>
+    <td>{{ priceConfig["비규격"]["12~50t"] }}원/kg</td>
+    <td>{{ priceConfig["중국산"]["12~50t"] }}원/kg</td>
+    <td>{{ priceConfig["SM275"]["12~50t"] }}원/kg</td>
+    <td>{{ priceConfig["SM355"]["12~50t"] }}원/kg</td>
+  </tr>
+  <tr>
+    <td>9t이하</td>
+    <td>{{ priceConfig["비규격"]["9t이하"] }}원/kg</td>
+    <td>{{ priceConfig["중국산"]["9t이하"] }}원/kg</td>
+    <td>{{ priceConfig["SM275"]["9t이하"] }}원/kg</td>
+    <td>{{ priceConfig["SM355"]["9t이하"] }}원/kg</td>
+  </tr>
+</tbody>
     </table>
     <p class="loss-note">※ 로스율 7% 적용</p>
 
@@ -43,7 +43,9 @@
   <!-- 스플라이스 철판을 두 번 보여주려면, 같은 값을 두 번 출력 -->
   <p>스플라이스 철판: {{ priceConfig.processingFee['스플라이스 철판'] }}원/kg</p>
   <p>일반 철판: {{ priceConfig.processingFee['일반 철판'] }}원/kg</p>
-  <p class="loss-note">※ 중량기준 : 사각환산중량(◪)</p>
+  <p class="loss-note">
+  ※ 중량기준 : 사각환산중량(<img src="@/assets/square.png" alt="square" class="square-icon" />)
+</p>
 </div>
 
   </div>
@@ -97,5 +99,10 @@ export default {
   margin: 20px 0;         /* 위아래 여백 */
   width: calc(100% + 40px); /* 좌우 padding 20px씩 보정 */
   margin-left: -20px;   
+}
+.square-icon {
+  width: 1em;      /* 글자 크기에 맞게 1em로 설정 */
+  height: auto;    /* 비율에 맞게 자동 조절 */
+  vertical-align: middle;  /* 텍스트와 수평 정렬 */
 }
 </style>
